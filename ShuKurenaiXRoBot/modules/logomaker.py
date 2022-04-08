6a5b1a0d3e8d997848840c4dafaa54aa9c0bfd03
mhdalthaf12 <92728565+mhdalthaf12@users.noreply.github.com>
2022-04-08 20:55:15 +0530

    Update logomaker.py

--- ShuKurenaiXRoBot/modules/logomaker.py
+++ ShuKurenaiXRoBot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ShuKurenaiXRoBot](https://t.me/ShuKurenaiXRoBot)",
+            caption="Logo by [єναмαяιуα](https://t.me/evamariyagbot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ShuKurenaiXRoBot](https://t.me/ShuKurenaiXRoBot)",
+            caption="Logo by [єναмαяιуα](https://t.me/evamariyagbot)",
             force_document=False,
         )
         os.remove(flnme)
